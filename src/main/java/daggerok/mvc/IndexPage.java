5171099f7023663ea760e87fccaa4f274d27e1f2
Maksim Kostromin <daggerok@gmail.com>
2018-06-27 04:02:17 +0300

    Initial commit

---
+++ src/main/java/daggerok/mvc/IndexPage.java
@@ -0,0 +1,30 @@
+package daggerok.mvc;
+
+import io.vavr.collection.List;
+
+import javax.inject.Inject;
+import javax.mvc.Models;
+import javax.mvc.annotation.Controller;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.TEXT_HTML;
+
+//tag::content[]
+@Path("")
+@Controller
+@Produces(TEXT_HTML)
+public class IndexPage {
+
+  @Inject Models models;
+
+  @GET
+  @Path("")
+  public String indexView() {
+    models.put("message", "Hello, World!");
+    models.put("data", List.of("ololo", "trololo").toJavaList());
+    return "index";
+  }
+}
+//end::content[]
