a5f18591daae0b40654f6e092612a4dda5115a29
Maksim Kostromin <daggerok@gmail.com>
2019-09-18 22:19:14 +0200

    Update versions... | The last time I tried this the monkey didn't sur… (#1)
    * Update versions... | The last time I tried this the monkey didn't survive. Let's hope it works better this time. (C) whatthecommit.com
    
    * Update... | should work I guess... (C) whatthecommit.com
    
    * Update. Again... | This is not a commit (C) whatthecommit.com
    
    * Update. Again... | Your commit is writing checks your merge can't cash. (C) whatthecommit.com
    
    * Update. Again... | i think i fixed a bug... (C) whatthecommit.com
    
    * Update. Again... | Just stop reading these for a while, ok.. (C) whatthecommit.com
    
    * Update. Again... | Derp search/replace fuckup (C) whatthecommit.com
    
    * Update. Finally | Derpy hooves (C) whatthecommit.com
    
    * Update. Finally | I was told to leave it alone, but I have this thing called OCD, you see (C) whatthecommit.com

--- src/main/webapp/WEB-INF/assets/scripts.js
+++ src/main/webapp/WEB-INF/assets/scripts.js
@@ -1,6 +1,8 @@
 (function main() {
   'use strict';
   window.document.addEventListener('DOMContentLoaded', function onDOMContentLoaded(evt) {
-    console.log('hola!');
+    console.log('materialize css initialization!');
+    M.AutoInit();
   }, false);
 })();
+
