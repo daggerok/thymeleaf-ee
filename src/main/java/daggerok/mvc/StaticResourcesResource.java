b3dd018cee0a2b9a420b80ebad08ff2f66f95a21
Maksim Kostromin <daggerok@gmail.com>
2019-09-18 23:56:34 +0300

    Testing in progress ;) (C) whatthecommit.com

--- src/main/java/daggerok/mvc/StaticResourcesResource.java
+++ src/main/java/daggerok/mvc/StaticResourcesResource.java
@@ -1,5 +1,7 @@
 package daggerok.mvc;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
@@ -8,11 +10,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
+import java.util.Objects;
 
 import static java.lang.String.format;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 //tag::content[]
+@Slf4j
 @Path("")
 @RequestScoped
 public class StaticResourcesResource {
@@ -28,11 +32,10 @@ public class StaticResourcesResource {
   @GET
   @Path("{path: ^webjars\\/.*}")
   public Response webjars(@PathParam("path") final String path) {
-
-    final String absolutePath = format("/META-INF/resources/%s", path);
-    final InputStream resource = getClass().getClassLoader().getResourceAsStream(absolutePath);
-
-    return null == resource
+    log.info("handling static resource: '{}'", path);
+    String absolutePath = format("/META-INF/resources/%s", path);
+    InputStream resource = getClass().getClassLoader().getResourceAsStream(absolutePath);
+    return Objects.isNull(resource)
         ? Response.status(NOT_FOUND).build()
         : Response.ok().entity(resource).build();
   }
@@ -49,9 +52,8 @@ public class StaticResourcesResource {
   @GET
   @Path("{path: ^(assets|public|static|resources)\\/.*}")
   public Response staticResources(@PathParam("path") final String path) {
-
-    final InputStream resource = context.getResourceAsStream(format("/WEB-INF/%s", path));
-
+    log.info("handling asset: '{}'", path);
+    InputStream resource = context.getResourceAsStream(format("/WEB-INF/%s", path));
     return null == resource
         ? Response.status(NOT_FOUND).build()
         : Response.ok().entity(resource).build();
